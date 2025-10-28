02aaafd384bbb2f02b39a712f6e26f0788f6fa69
Sushant <sushant.dhiman9812@gmail.com>
2025-10-28 12:38:15 +0530

    feat: Added functionality to specify total lines to read

--- tael.c
+++ tael.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DEFAULT_LINES 10
 
@@ -12,14 +13,14 @@ void print_error_with_instructions(char* error, char* instructions) {
     printf("\n%s\n", instructions);
 }
 
-int read_file(FILE* file, unsigned int lines_to_read) {
+int read_file(FILE* file, const unsigned int lines_to_read) {
 
     if(fseek(file, 0, SEEK_END) != 0) {
         fclose(file);
         return 1;
     }
 
-    long file_size = ftell(file);
+    const long file_size = ftell(file);
     if (file_size == -1L) {
         fclose(file);
         return 1;
@@ -39,7 +40,7 @@ int read_file(FILE* file, unsigned int lines_to_read) {
             return 1;
         }
 
-        int ch = fgetc(file);
+        const int ch = fgetc(file);
         if (ch == EOF) break;
 
         if (ch == '\n') {
@@ -50,17 +51,15 @@ int read_file(FILE* file, unsigned int lines_to_read) {
         pos--;
     }
 
-    long start_pos = (new_line_count == lines_to_read) ? pos + 1 : 0;
+    const long start_pos = new_line_count == lines_to_read ? pos + 1 : 0;
 
     if(fseek(file, start_pos, SEEK_SET) != 0) {
         fclose(file);
         return 1;
     }
 
-    int ch;
-
     while(1) {
-        ch = fgetc(file);
+        const int ch = fgetc(file);
         if(ch == EOF) {
             putchar('\n');
             break;
@@ -77,24 +76,43 @@ int read_file(FILE* file, unsigned int lines_to_read) {
 /*
     Usage:
     tael <file_name> - Displays last 10 lines
+    tael <file_name> -<line_count> - Displays given lines.
 */
 
-int main(int argc, char** argv) {
+int main(const int argc, char** argv) {
     if (argc < 2) {
         char* inst = "Usage: tael <file_name> or run tael --help for more information.";
         print_error_with_instructions("Invalid Command", inst);
         return 0;
     }
 
-    char* file_name = argv[1];
+    const char* file_name = argv[1];
+    int lines_to_read = DEFAULT_LINES;
+
+    // Handles tael <file_name> -<line_count> by calculating lines if specified
+    if (argc == 3) {
+        const char* c_lines = argv[2];
+        if (c_lines != 0) {
+            const int i_lines = abs(atoi(c_lines));
+            if (i_lines == 0) {
+                char* inst = "Usage: tael <file_name> -<lines>";
+                print_error_with_instructions("Invalid Command", inst);
+                return 0;
+            }
+
+            lines_to_read = i_lines;
+        }
+    }
+
 
     FILE* file = fopen(file_name, "rb");
     if (!file) {
-        print_error("File not found");
+        print_error("File not found.");
         return 1;
     }
 
-    if(read_file(file, DEFAULT_LINES) != 0) {
+    printf("Lines to read: %d", lines_to_read);
+    if(read_file(file, lines_to_read) != 0) {
         print_error("Unable to read file.");
         return 1;
     }
